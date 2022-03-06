bdd948191f903862ac4d8b75ac0cece2fa4513b1
Natanael Nascimento <7495876+natanaelnascimento@users.noreply.github.com>
2022-03-06 12:34:48 -0300

    Criação de repositório específico para o front

--- client/src/services/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-//Define a URL base da origem para consumo do servico
-export default axios.create({
-  baseURL: 'https://transactions-nsn.herokuapp.com/api',
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
